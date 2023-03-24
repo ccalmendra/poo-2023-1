5540f403d28afbd4f26987cc7fb4494870687e80
Camilo Almendra <camilo.almendra@ufc.br>
2023-03-24 10:20:40 -0300

    Add files via upload

---
+++ modularidade/jogo-pedra-papel-tesoura/PedraPapelTesoura.java
@@ -0,0 +1,40 @@
+
+// https://docs.oracle.com/en/java/javase/20/docs/api/java.base/java/util/Random.html
+import java.util.Random;
+
+
+public class PedraPapelTesoura {
+	
+	static final int PEDRA = 0;
+	static final int PAPEL = 1;
+	static final int TESOURA = 2;
+ 
+	static public void main(String[] args){
+		System.out.println("Vamos jogar Pedra, Papel, Tesoura!");
+		int jogadaUsuario = recebeJogadaUsuario();
+		int jogadaCpu = geraJogadaCpu();
+		int resultado = executaJogada(jogadaUsuario, jogadaCpu);
+		System.out.println("cpu = " + jogadaCpu);
+		exibeResultado(jogadaUsuario, jogadaCpu, resultado);
+	}
+	
+	static public int recebeJogadaUsuario(){
+		return 0;
+	}
+	
+	static public int geraJogadaCpu(){
+		Random rnd = new Random();
+		long semente = System.currentTimeMillis();
+		rnd.setSeed(semente);
+		return rnd.nextInt(3); // retorna aleatorio entre 0 e 2
+	}
+	
+	static public int executaJogada(int jogadaUsuario, int jogadaCpu){
+		return 0;
+	}
+	
+	static public void exibeResultado(int jogadaUsuario, int jogadaCpu, int resultado){
+		
+	}
+	
+}
